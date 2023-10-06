af4667da4d201fb82aa5cc5f49177873cc43e63a
Rayster Ramos <rjrfrst@gmail.com>
2023-10-06 12:29:28 +0100

    Second commit, add refactored solutions

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class SecretNumber {
 
     public static void main(String[] args) {
-        System.out.println("Guess the secret number");
+        System.out.println("Guess the secret number, between 1 and 9");
 
         Scanner reader = new Scanner(System.in);
         int secretNumber = reader.nextInt();
 
-//        If input is correct, output is equals to correct
+//        If input is correct, they guessed correctly
         if (secretNumber == 6){
             System.out.println("Congratulations you guessed it");
-        } else if (secretNumber <= 6){
+        } else if (secretNumber < 6){
             System.out.println("Too low, guess higher");
         } else{
             System.out.println("Too high, guess lower");
         }
+
     }
+
 }
